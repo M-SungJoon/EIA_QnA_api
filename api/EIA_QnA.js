8db2f5329d7f355962d27ad4af52571d0ab81948
M-SungJoon <munhoney0401@gmail.com>
2025-07-15 17:00:48 +0900

    Update EIA_QnA.js

--- api/EIA_QnA.js
+++ api/EIA_QnA.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   const { searchword } = req.query;
-  const serviceKey = encodeURIComponent('gl44IFQw8C271zsqN8GL5vBnm%2Fb9Fv8KVY7bvyrKJeApitaZawOwcmXMe3PIumRHLvN2Arg5cWK%2BBgnapNWxOQ%3D%3D'); // URL 인코딩된 키
+  const serviceKey = encodeURIComponent(process.env.SERVICE_KEY); // URL 인코딩된 키
 
   const url = `http://apis.data.go.kr/1140100/minAnalsInfoView5/minSimilarInfo5?serviceKey=${serviceKey}&retCount=20&minScore=2&startPos=1&dataType=json&target=qna_origin${searchword ? `&searchword=${encodeURIComponent(searchword)}` : ''
   }`;
