f06a816e02adf0fac3955e0338624a3b89d71b9f
M-SungJoon <munhoney0401@gmail.com>
2025-07-16 13:51:05 +0900

    Update EIA_QnA_List.js

--- api/EIA_QnA_List.js
+++ api/EIA_QnA_List.js
@@ -5,7 +5,14 @@ module.exports = async (req, res) => {
   const { keyword } = req.query;
   const serviceKey = encodeURIComponent(process.env.SERVICE_KEY); // URL 인코딩된 키
 
-  const url = `http://apis.data.go.kr/1140100/CivilPolicyQnaService/PolicyQnaList?serviceKey=${serviceKey}&firstIndex=1&recordCountPerPage=20&regFrom=20200101&regTo=20250715${keyword ? `&keyword=${encodeURIComponent(keyword)}` : ''
+  // 오늘 날짜를 yyyymmdd 형식으로 변환
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const dd = String(today.getDate()).padStart(2, '0');
+  const regTo = `${yyyy}${mm}${dd}`;
+	
+  const url = `http://apis.data.go.kr/1140100/CivilPolicyQnaService/PolicyQnaList?serviceKey=${serviceKey}&firstIndex=1&recordCountPerPage=20&regFrom=20200101&regTo=${regTo}${keyword ? `&keyword=${encodeURIComponent(keyword)}` : ''
   }`;
 
 	try {
